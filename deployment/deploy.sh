a042945992d8c3fa452540bd55fce322ecacf51a
Chaudhary, Mahima <Mahima.Chaudhary@united.com>
2022-09-20 11:08:40 -0400

    change main class in pom file

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -2,7 +2,7 @@
 
 java -jar $1 \
 --debug.logging.enabled false \
---file.location /home/sanket/Documents/workspace/datasplit_java/src/main/resources/data/iris.txt \
+--file.location /Users/mahima.chaudhary/IdeaProjects/datasplit/src/main/resources/data/iris.txt \
 --train.test.percentage 0.8 \
---train.output.location /home/sanket/Documents/workspace/datasplit_java/src/main/resources/data/irisTrain.txt \
---test.output.location /home/sanket/Documents/workspace/datasplit_java/src/main/resources/data/irisTest.txt
+--train.output.location /Users/mahima.chaudhary/IdeaProjects/datasplit/src/main/resources/data/irisTrain.txt \
+--test.output.location /Users/mahima.chaudhary/IdeaProjects/datasplit/src/main/resources/data/irisTest.txt
