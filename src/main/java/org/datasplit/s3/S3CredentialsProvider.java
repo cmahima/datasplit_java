92bda23c58d40a019bacb45dfebcfa7f22063b45
Chaudhary, Mahima <Mahima.Chaudhary@united.com>
2022-09-22 11:22:07 -0400

    add regions in AmazonS3ClientBuilder

--- src/main/java/org/datasplit/s3/S3CredentialsProvider.java
+++ src/main/java/org/datasplit/s3/S3CredentialsProvider.java
@@ -4,6 +4,7 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.datasplit.domain.S3Credentials;
@@ -14,7 +15,7 @@ public class S3CredentialsProvider implements AWSCredentialsProvider {
     public AmazonS3 s3Client;
     public S3CredentialsProvider(S3Credentials credentials){
          this.awsCreds = new BasicAWSCredentials(credentials.getAWSAccessKeyId(), credentials.getAWSSecretKey());
-         this.s3Client = AmazonS3ClientBuilder.standard()
+         this.s3Client = AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION)
                 .withCredentials(new AWSStaticCredentialsProvider(this.awsCreds))
                 .build();
     }
